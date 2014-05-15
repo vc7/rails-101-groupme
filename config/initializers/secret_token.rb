c303c6bb956e302a49d8176515fa9e5ef2c95d43
Vincent Chen <vince78718@gmail.com>
2014-05-15 19:26:06 +0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails101Groupme::Application.config.secret_key_base = 'a972d066963b65816916bfc828298b54ea7d3468c9145c4bf8ffdea7f713cd2de8cb4db393210a72f78c93d6455e7d5386cc597314bc94792c4ea897a953e408'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails101Groupme::Application.config.session_store :cookie_store, key: '_rails-101-groupme_session'
