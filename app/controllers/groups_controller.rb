adae3663e7887760a1a70ad6dc19c03c93e5222f
Vincent Chen <vince78718@gmail.com>
2014-05-22 00:19:39 +0800

    complete ch1.1.6

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -26,6 +26,17 @@ class GroupsController < ApplicationController
 		@group = Group.find(params[:id])
 	end
 
+	# process the updated data from edit view
+	def update
+		@group = Group.find(params[:id])
+
+		if @group.update(group_params)
+			redirect_to group_path(@group)
+		else
+			render :edit
+		end
+	end
+
 	private
 
 	def group_params
